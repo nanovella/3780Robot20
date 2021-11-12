73a60d50be93184903c95b64cb9d4e84df2f89a4
nanovella <doublevolt33@gmail.com>
2021-11-12 14:00:52 -0500

    yes

--- milestone_1G20/milestone_1G20.ino
+++ milestone_1G20/milestone_1G20.ino
@@ -8,37 +8,36 @@
 #define RFmotor 10
 #define RBmotor 9
 #define delaytime 20
-
+#define SPEED 100
 
 // declare any variables here
-int delay_time_1 = 1000;
-int delay_time_2 = 1000;
+int delay_time_1 = 4000;
+int delay_time_2 = 2000;
 
 int main(void){ 
   // setup code that only happens once
   // use the DDR registers to set the appropriate pins as outputs
   // Left Motor
-  DDRD = 0b01101000;
-  TCCR0A = 0b00100001; 
-  TCCR0B = 0b00000110;
-  OCR0A = 0;
-  OCR0B = 0;
-  TIMSK1 = 0b00000010;
+  Serial.begin(9600);
+  DDRD = 0b00001000;
+  DDRB = 0b00001110;
+  TCCR2A = 0b00100001; 
+  TCCR2B = 0b00000110;
   //Right Motor
-  DDRB = 0b00000110;
   TCCR1A = 0b00100001; 
-  TCCR1B = 0b00000110;
-  OCR1A = 255;
-  OCR1B = 255;
-  TIMSK1 = 0b00000010;
+  TCCR1B = 0b00000100;
   sei();
   // sequence for milestone 1
   // call the functions in the correct order with delays either in between the function calls or in the
   // functions themselves (see example code on Canvas for using functions).
   // this example would have the robot drive forward for 1 second and then stop. 
-
-  drive_forward();
-  _delay_ms(delay_time_1); // _delay_ms() is the delay command in C (not the same as the Arduino delay() command)
+  Serial.println("forward");
+  drive_backward();
+  _delay_ms(delay_time_1);
+  stop_robot();
+  Serial.println("right");
+  turn_right();
+  _delay_ms(delay_time_1);
   stop_robot();
 
   while(1){
@@ -56,43 +55,32 @@ int drive_forward(void){
   // change the value of OCR2B to change the PWM duty cycle
   // this example will just oscillate between 0% (0) and 100% (255) duty cycle
   // spin both wheels forward
-    while(OCR0A<255){
-      OCR0A++; // increment output compare register
-      OCR1A++;
-      _delay_ms(delaytime);
-    }
+      OCR2B = SPEED;
+      OCR1B = SPEED;
+ 
 }
 
 int drive_backward(void){
   // spin both wheels backward
-    while(OCR0B<255){
-      OCR0B++; // increment output compare register
-      OCR1B++;
-      _delay_ms(delaytime);
-    }
+      OCR2A = SPEED;
+      OCR1A = SPEED;
 }
 
 int turn_left(void){
   // spin left wheel backward, right wheel forward
-    while(OCR0B<255){
-      OCR0B++; // increment output compare register
-      OCR1A++;
-      _delay_ms(delaytime);
-    }
+      OCR2A = SPEED;
+      OCR1B = SPEED;
 }
 
 int turn_right(void){
   // spin right wheel backward, left wheel forward
-    while(OCR0B<255){
-      OCR0A++; // increment output compare register
-      OCR1B++;
-      _delay_ms(delaytime);
-    }
+      OCR2B = SPEED;
+      OCR1A = SPEED;
 }
-}
+
 
 int stop_robot(void){
   // stop both wheels
-  PORTD &= 0b10011111;
-  PORTB &= 0b11111001;
+      OCR2B = 0;
+      OCR1B = 0;
 }
